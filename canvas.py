d108e769e34ae87858ff7bcdc4eba1c099eb8a21
Stefan Versan <46283215+xande30@users.noreply.github.com>
2023-01-31 13:47:23 +0100

    Add files via upload

---
+++ canvas.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from colorama import Fore
+
+canvas = Canvas(width=525,height=300, bg='grey')
+canvas.pack(expand=YES, fill=BOTH)
+
+canvas.create_line(100, 100, 200, 200)
+canvas.create_line(100,200,200,300)
+for i in range(1,20,2):
+    canvas.create_line(0, i, 50, i)
+
+canvas.create_oval(10,10, 200,200, width=2, fill='yellow')
+canvas.create_arc(200,200, 300,100)
+canvas.create_rectangle(200,200,300,300, width=5, fill='blue')
+canvas.create_line(0, 300,150,150, width=10, fill='green')
+
+photo = PhotoImage(file='../pythonProject/nice.GIF')
+canvas.create_image(325, 25, image=photo, anchor=NW)
+
+widget= Label(canvas, text='Demo', fg='red', bg='black')
+widget.pack()
+canvas.create_window(100,100,window=widget)
+canvas.create_text(100,280, text='HELLO')
+mainloop()
